3da546ad9c177d882a806875877ae2069ad77649
eweeks <eweeks@g.harvard.edu>
2014-10-01 13:06:02 -0600

    minor change to footer

--- footer.php
+++ footer.php
@@ -5,8 +5,7 @@
 <li>All photos taken by Carl H. Calhoun</li>
 =======
 <li>All photos taken by Carl Calhoun</li>
->>>>>>> 84da49611d0515cdac626c4e534ff34b93470dbe
-<li><?php echo 'Document last modified '. date ("F d Y H:i:s.", getlastmod());?> </li>
+<li><?php  echo 'Document last modified '. date ("F d Y.", getlastmod());?> </li>
 <li class="small">Website by Esther Weeks</li>
 </ul>
 </div>
