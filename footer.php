4e69c9d4e6286ed26c133995500022ded7ad9373
eweeks <eweeks@g.harvard.edu>
2014-10-01 12:24:44 -0600

    test

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <div id="footer">
 <ul>
 <li><a href="index.php" title="Return to the homepage">Home</a></li>
-<li>All photos taken by Carl Calhoun</li>
+<li>All photos taken by Carl H. Calhoun</li>
 <li><?php echo 'Document last modified '. date ("F d Y H:i:s.", getlastmod());?> </li>
 <li class="small">Website by Esther Weeks</li>
 </ul>
